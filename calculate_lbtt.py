56d9fc7ac2db226ce5c7f84103d8c3d735851308
John Soto <scruffy403@gmail.com>
2022-08-10 11:28:54 +0100

    added claculate_lbtt_previous_buyer function to calculate lbtt for previous home owner

--- calculate_lbtt.py
+++ calculate_lbtt.py
@@ -9,10 +9,9 @@ def calculate_lbtt_first_time_buyer(property_value):
     pass
 
 def calcultate_lbtt_previous_buyer(property_value):
-    price_bands = [PurchasePriceBands(0, 145000, 0), PurchasePriceBands(145001, 250000, 0.02), PurchasePriceBands(250001, 325000, 0.06), PurchasePriceBands(325001, 750000, 0.10), PurchasePriceBands(750001, float(inf), 0.12)]
+    price_bands = [PurchasePriceBands(0, 145000, 0), PurchasePriceBands(145001, 250000, 0.02), PurchasePriceBands(250001, 325000, 0.05), PurchasePriceBands(325001, 750000, 0.10), PurchasePriceBands(750001, float('inf'), 0.12)]
     bands = []
     tax = 0
-
 
     for band in price_bands:
         if band.low < property_value:
@@ -25,4 +24,8 @@ def calcultate_lbtt_previous_buyer(property_value):
         else:
             tax += (property_value - band.low)*band.rate
     
-    return round(tax, 2)
+    return round(tax)
+
+property_value = float(input("Enter home price: "))
+tax = calcultate_lbtt_previous_buyer(property_value)
+print(tax)
